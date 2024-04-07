8f4141f1bc4cf1c246078753f98306994ed30de9
Jasper Lorenz <jasper@lorenzmail.de>
2024-04-07 13:22:48 +0200

    Fixes existing file renaming for recrods and adds store/load command for recordings to disk. Also prevents mouse from going to locked mode when menu is closed in replay when preventing menu

--- src/main/java/net/jasper/mod/mixins/menuprevention/PreventSetScreen.java
+++ src/main/java/net/jasper/mod/mixins/menuprevention/PreventSetScreen.java
@@ -4,6 +4,8 @@ import net.jasper.mod.automation.MenuPrevention;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,8 +15,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class PreventSetScreen {
     @Inject(method="setScreen", at=@At("HEAD"), cancellable=true)
     private void injected(Screen screen, CallbackInfo ci) {
-        if (MenuPrevention.preventToBackground && screen instanceof GameMenuScreen) {
-            ci.cancel();
+        if (MenuPrevention.preventToBackground) {
+            MinecraftClient client = MinecraftClient.getInstance();
+            // When closing a window enable cursor again and if trying to open menu cancel callback
+            if (screen == null) {
+                InputUtil.setCursorParameters(client.getWindow().getHandle(), GLFW.GLFW_CURSOR_NORMAL, client.mouse.getX(), client.mouse.getY());
+            } else if (screen instanceof GameMenuScreen) {
+                ci.cancel();
+            }
         }
     }
 }
