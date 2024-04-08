84603249b12193fc15f3a0befdd38dcb7966f593
Jasper Lorenz <jasper@lorenzmail.de>
2024-04-08 12:32:48 +0200

    Implements KeyBindingAccessor

--- src/main/java/net/jasper/mod/mixins/KeyBindingAccessor.java
+++ src/main/java/net/jasper/mod/mixins/KeyBindingAccessor.java
@@ -1,2 +1,12 @@
-package net.jasper.mod.mixins;public interface KeyBindingAccessor {
+package net.jasper.mod.mixins;
+
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(KeyBinding.class)
+public interface KeyBindingAccessor {
+    @Accessor("boundKey")
+    InputUtil.Key getBoundKey();
 }
