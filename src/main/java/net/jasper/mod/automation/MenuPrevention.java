0ab6e03473e18bad78559b0417b3f15bd2a42fcb
Jasper Lorenz <jasper@lorenzmail.de>
2024-04-09 10:19:08 +0200

    Adds menu prevention icon to screens as overlay and prevents keyboard action except turn menu prevention off when active

--- src/main/java/net/jasper/mod/automation/MenuPrevention.java
+++ src/main/java/net/jasper/mod/automation/MenuPrevention.java
@@ -3,9 +3,12 @@ package net.jasper.mod.automation;
 import net.jasper.mod.gui.option.PlayerAutomaOptionsScreen;
 import net.jasper.mod.util.ClientHelpers;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
+
+import static net.jasper.mod.util.HUDTextures.BLOCK_MENU_ICON;
 
 public class MenuPrevention {
 
@@ -14,6 +17,21 @@ public class MenuPrevention {
 
     public static void register() {
         registered = true;
+    }
+
+    public static void renderIcon(DrawContext context) {
+        // Rendering the texture to show that menu opening is blocked anda the mouse can be used freely
+        // This is always on and should never be effected by showHUDOption
+        if (preventToBackground) {
+            MinecraftClient client = MinecraftClient.getInstance();
+            context.getMatrices().push();
+            // Texture is 24x24. Scale it with guiScale
+            int scaledSizeBlockMenu = 24 * ClientHelpers.getGuiScale();
+            int xBlockMenu = client.getWindow().getScaledWidth() / 2 - scaledSizeBlockMenu / 2;
+            int yBlockMenu = client.getWindow().getScaledHeight() / 2 - scaledSizeBlockMenu / 2;
+            context.drawTexture(BLOCK_MENU_ICON, xBlockMenu, yBlockMenu, 0, 0, scaledSizeBlockMenu, scaledSizeBlockMenu, scaledSizeBlockMenu, scaledSizeBlockMenu);
+            context.getMatrices().pop();
+        }
     }
 
     public static void toggleBackgroundPrevention() {
